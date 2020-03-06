47b0a0cba9b6ba0df36765335ccc613c13ecd750
Maxim Konovalov <maksik852002@gmail.com>
2020-03-06 14:35:33 +0600

    add authMiddleware

--- music-api/app/track_history.js
+++ music-api/app/track_history.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const TrackHistory = require("../models/TrackHistory");
-const User = require("../models/User");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const AuthHeader = req.get("Authorization");
-  if (!AuthHeader) {
-    return res.status(401).send({ error: "User must be logged in" });
-  };
-  const [type, token] = AuthHeader.split(' ');
-  if (type !== 'Token' || !token) {
-    return res.status(401).send({ error: "User must be logged in" });
-  };
-  const user = await User.findOne({ token });
-  if (!user) {
-    return res.status(401).send({ error: "User must be logged in" });
-  };
-
   try {
     const date = new Date();
     const history = new TrackHistory(req.body);
-    history.userId = user._id;
     history.datetime = date;
     await history.save();
-    res.send(history);
+    return res.send(history);
   } catch (e) {
-    res.status(400).send(e);
+    return res.status(400).send(e);
   }
-  
 });
 
 module.exports = router;
--- music-api/index.js
+++ music-api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const autorization = require('./authMiddleware');
 
 const artists = require("./app/artists");
 const albums = require("./app/albums");
@@ -14,6 +15,7 @@ const port = 8000;
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
+app.use('/track_history', autorization);
 
 const run = async () => {
   await mongoose.connect("mongodb://localhost/musicApp", {
